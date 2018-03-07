bb8750930dd729f5364736639654df0d856cb9f7
J-Mangia <j.mangia@ibm.com>
2018-03-06 17:34:53 -0800

    Refactor Package name

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "thrift-swift-nio",
+    name: "ThriftSwiftNio",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "thrift-swift-nio",
-            targets: ["thrift-swift-nio"]),
+            name: "ThriftSwiftNio",
+            targets: ["ThriftSwiftNio"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "thrift-swift-nio",
+            name: "ThriftSwiftNio",
             dependencies: ["NIO", "NIOHTTP1", "Thrift"]),
         .testTarget(
-            name: "thrift-swift-nioTests",
-            dependencies: ["thrift-swift-nio"]),
+            name: "ThriftSwiftNioTests",
+            dependencies: ["ThriftSwiftNio"]),
     ]
 )
